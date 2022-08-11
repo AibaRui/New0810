a2b4a819f9474e413f8f2a704e4792cd6b9a04d3
DESKTOP-T8MHPRI\vantan <kou8866m@gmail.com>
2022-08-11 14:35:35 +0900

    ｐ

--- Assets/MORISHIMA/Scripts/TITLE.cs
+++ Assets/MORISHIMA/Scripts/TITLE.cs
@@ -1,14 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class TITLE : MonoBehaviour
 {
 
     public void OnClick()
     {
-        SceneManager.LoadScene("TITLEScene");
+        FadeManager.Instance.LoadScene("Title",1.0f);
     }
 
 }
